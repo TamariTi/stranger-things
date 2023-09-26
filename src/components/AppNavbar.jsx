037c57d384609ce48f6e76e46c58f8a5a8210c9a
Tavion Boc <tavionboc@Tavions-MBP.lan>
2023-09-25 19:21:32 -0700

    u

---
+++ src/components/AppNavbar.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+function AppNavbar() {
+  return (
+    <>
+      <Navbar className='nava'>
+        <Container>
+          <Navbar.Brand href="/">Stranger Things</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Posts</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/create">Create post</Nav.Link>
+            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default AppNavbar;
